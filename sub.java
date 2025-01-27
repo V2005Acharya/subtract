f9aa8a1f5e534973e9c38b5d972c62306928dd35
V2005Acharya <124271713+V2005Acharya@users.noreply.github.com>
2025-01-27 01:48:41 -0800

    Update sub.java

--- sub.java
+++ sub.java
@@ -2,10 +2,10 @@ class sub
   {
     public static void main(string args[])
   {
-    int a,b,sub,
-    a=9;
-    b=4;
-    sub=a-b;
+     int a,b,sub,
+     a=9;
+     b=4;
+     sub=a-b;
   System.out.println("subtraction of 2 number is"+sub);
   }
 }
